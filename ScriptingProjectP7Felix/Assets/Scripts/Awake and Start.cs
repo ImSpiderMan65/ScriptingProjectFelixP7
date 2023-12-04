79e9ec930ed91d4f8e68f26666e9aa579e799b26
Miguel Felix Cazarez <755699@student.dusd.net>
2023-12-04 14:31:48 -0800

    awake
    awakew

---
+++ ScriptingProjectP7Felix/Assets/Scripts/Awake and Start.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    void Start()
+    {
+        Debug.Log("Start called.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
